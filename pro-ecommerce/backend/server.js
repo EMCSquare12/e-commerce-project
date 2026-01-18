02435a9d89a2536eca77f7ca4e27b0ca326a5022
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2026-01-18 18:39:07 +0800

    Add cors configuration

--- pro-ecommerce/backend/server.js
+++ pro-ecommerce/backend/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'; 
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-// 1. Import routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -14,25 +14,31 @@ import adminRoutes from './routes/adminRoutes.js';
 import imageRoutes from './routes/imageRoutes.js';
 import notificationsRoutes from './routes/notificationsRoutes.js';
 
-
-
-
 connectDB();
 
 const app = express();
 
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://pro-shop-ecommerce.netlify.app/",
+        "https://pro-shop-ecommerce-admin.netlify.app/"
+
+    ],
+    credentials: true
+}));
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Cookie parser middleware
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// 2. Mount routes
+// Mount routes...
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -41,8 +47,7 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/upload', imageRoutes);
 app.use('/api/notifications', notificationsRoutes);
 
-
-// Error Handling
+// Error Handling...
 app.use(notFound);
 app.use(errorHandler);
 
