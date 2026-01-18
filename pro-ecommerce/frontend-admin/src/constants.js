d5ea75ff21f01d3db5af419f912105d0aba1f21a
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2026-01-18 12:10:36 +0800

    Added vite config for constant

--- pro-ecommerce/frontend-admin/src/constants.js
+++ pro-ecommerce/frontend-admin/src/constants.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = import.meta.env.VITE_API_URL || ''; 
-export const BASE_URL = ''; // Uses proxy
+export const BASE_URL = import.meta.env.VITE_API_URL || '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
