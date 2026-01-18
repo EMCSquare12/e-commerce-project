0a4684fd2a4745dc3a769adc5532d54421251ec2
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2026-01-18 19:58:05 +0800

    changes

--- pro-ecommerce/frontend-client/src/components/StripeWrapper.jsx
+++ pro-ecommerce/frontend-client/src/components/StripeWrapper.jsx
@@ -4,7 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Outlet } from "react-router-dom";
 
 // Public Key (Safe to expose)
-const stripePromise = loadStripe("pk_test_51SYgfjF8qMkexvO6kWhQyVYUK82v20Fb5odLC327oL3QnU2bcK5Uriy9ov5xCuPIp8vIV6yXId1rOOBFiCKGwM2N00g8EvrAff");
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const StripeWrapper = () => {
   return (
