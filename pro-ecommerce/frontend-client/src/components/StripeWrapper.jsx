b85b05b8fa69dae7a57e613918b21b4785e76e23
Erniel Caalim <133772245+EMCSquare12@users.noreply.github.com>
2026-01-18 19:06:31 +0800

    Fix: Stripe Publishable Key

--- pro-ecommerce/frontend-client/src/components/StripeWrapper.jsx
+++ pro-ecommerce/frontend-client/src/components/StripeWrapper.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 // Public Key (Safe to expose)
 const stripePromise = loadStripe(
-  "pk_test_51SYgfjF8qMkexvO6kWhQyVYUK82v20Fb5odLC327oL3QnU2bcK5Uriy9ov5xCuPIp8vIV6yXId1rOOBFiCKGwM2N00g8EvrAff"
+  import.meta.env.VITE_STRIPE_PUBLIC_KEY
 );
 
 const StripeWrapper = () => {
